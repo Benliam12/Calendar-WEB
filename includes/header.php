b6148dc27db8f67c13384ec1c8ff096046221d4e
Benliam12 <s100live.ca@live.ca>
2018-10-06 19:18:33 -0400

    CREATION DE LA MORT QUI TUE

---
+++ includes/header.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Benliam12
+ * Date: 2018-09-30
+ * Time: 23:03
+ */
+?>
+<!doctype html>
+<html>
+    <head>
+        <title>Calendar</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Benliam12
+ * Date: 2018-09-30
+ * Time: 22:55
+ */
+require_once('includes/header.php');
+?>
+
+
+<?php
+require_once('includes/footer.php');
+?>
+
+
